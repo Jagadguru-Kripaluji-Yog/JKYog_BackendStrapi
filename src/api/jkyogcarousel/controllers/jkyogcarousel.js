53a2cb0db26aeeacbb3dc780446ac1b8a2faa525
Satya Prakash <satya.prakash@jaiinfoway.com>
2023-08-25 18:19:43 +0530

    delete unwanted collection

---
+++ src/api/jkyogcarousel/controllers/jkyogcarousel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * jkyogcarousel controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::jkyogcarousel.jkyogcarousel');
