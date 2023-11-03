2531303140e087a0502f1eb420d18c73d55611dc
SergeyBog <61414095+SergeyBog@users.noreply.github.com>
2023-11-03 16:38:27 +0200

    added new home order model

---
+++ src/api/apphomepagecontentorder/controllers/apphomepagecontentorder.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * apphomepagecontentorder controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::apphomepagecontentorder.apphomepagecontentorder');
