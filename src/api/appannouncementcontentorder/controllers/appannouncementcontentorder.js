f9116c01862882caa143c3e9134b13de1a948c32
SergeyBog <61414095+SergeyBog@users.noreply.github.com>
2023-11-06 16:43:22 +0200

    add new scheme + rename two fields

---
+++ src/api/appannouncementcontentorder/controllers/appannouncementcontentorder.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * appannouncementcontentorder controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::appannouncementcontentorder.appannouncementcontentorder');
---
+++ src/api/appannouncementcontentorder/routes/appannouncementcontentorder.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * appannouncementcontentorder router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::appannouncementcontentorder.appannouncementcontentorder');
