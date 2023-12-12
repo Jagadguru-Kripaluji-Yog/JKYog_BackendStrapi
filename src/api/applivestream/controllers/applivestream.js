ebac3fcd8d712fbd7f86d46949bcfc57b9fbb689
SergeyBog <61414095+SergeyBog@users.noreply.github.com>
2023-12-12 14:55:12 +0200

    added new live stream scheme

---
+++ src/api/applivestream/controllers/applivestream.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * applivestream controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::applivestream.applivestream');
